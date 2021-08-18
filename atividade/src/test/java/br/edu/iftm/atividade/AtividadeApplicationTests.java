23d518aab9f9bc9e2f9210921f5b8e2096c1054b
Patrícia Rocha <patriciamsro@gmail.com>
2021-08-18 20:21:27 -0300

    Projeto criado

---
+++ atividade/src/test/java/br/edu/iftm/atividade/AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
